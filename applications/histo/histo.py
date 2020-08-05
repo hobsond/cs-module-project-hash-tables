7d77900c997910dc0bc8470c22f83a67bf9e2f35
hobsond <60270270+hobsond@users.noreply.github.com>
2020-08-05 18:38:18 -0400

    Update histo.py

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,45 @@
 # Your code here
 
+
+def histo(file):
+    # open the text
+    cache = {}
+    textFile = open(f"{file}.txt",'r')
+    lineArray = textFile.read()
+    current = ''
+    for i in lineArray:
+        if i.isalpha():
+            current += i
+            
+        if i.isspace():
+            current = current.lower()
+            # x[current] = current
+            x = current
+            current=''
+            
+            if x in cache:
+                cache[x] += '*'
+            else:
+                cache[x] = ""
+                
+        
+    return cache          
+            
+    # print(x)
+    
+    
+    
+def sortPrint(item):
+   j =  histo(item)
+   
+   item = list(j.items())
+   item.sort(key=lambda e: e[1],reverse=False)
+   for i in item:
+       if len(i[1]) > 0:
+           print(f"{i[0]} {i[1]} " ) 
+           
+        
+   
+    
+    
+sortPrint('robin')
